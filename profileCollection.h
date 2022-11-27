5f0ccb715b55caf44ac84d18f77d5aed600cd92f
Ethan <19elb4@queensu.ca>
2022-11-27 15:33:52 -0600

    profile code V1.1 - profileCollection and profile class now include verification, and have SQLite database code included. Still missing getter for profile info from SQL datavase

--- profileCollection.h
+++ profileCollection.h
@@ -13,9 +13,22 @@ using namespace std;
 
 class profileCollection {
 
+    //how do we initalize database?
+
     int create_profileDB(const char *dbName);
     int createProfileTable();
-    int storeToProfileDB(profile profile);
+    int storeToProfileDB(profile profile); //how is username passed? alter user create to include storeToPRofile?
+
+    //function that gets all fields for use by website:
+    /*
+     * takes in email and password.
+     * gets username of related email and password
+     *
+     * gets username ascoiated with this email and password
+     * gets all posts made by user with ascociated username
+     *
+     * returns posts and usernme in form to be handled by websockets/javascript
+     */
 
 
 };
