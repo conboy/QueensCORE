d3b43fc0d2e087afa7a18d375142819f7dd98d5b
Conrad Fernandez <conjamalex@gmail.com>
2022-11-28 21:31:25 -0500

    Implemented sign in on the frontend

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -1,24 +1,20 @@
-import {useState } from 'react';
-
-
 const SignIn = () => {
-//  going to be using state hooks to handle input 
+    const ws =  new WebSocket('ws://127.0.0.1:2236');
+
+    function inputToString(document) {
+        return "login:"+document.getElementById("email").value+":"+document.getElementById("password").value;
+    }
+
     return ( 
         <div className="signin">
             <div className="Header"><h1>Sign in</h1></div>
             <h3>Create Account</h3>
-            
             <div className="form">
-                
-                    <form>
-                        <h3>Enter email:</h3>
-                         <input type="text" placeholder='netID@queensu.ca' />
-                    </form>
-                
-                    <form>
-                        <h3>Enter password:</h3>
-                         <input type="text" placeholder='Password'/>
-                    </form>                    
+                <h3>Enter email:</h3>
+                <input id="email" type="text" placeholder='netID@queensu.ca' />
+                <h3>Enter password:</h3>
+                <input id="password" type="text" placeholder='Password'/>
+                <button type="submit" onClick = {() => ws.send(inputToString(document))}>Sign In</button>
             </div>
             
         </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// for websocket implementation
+import WebSocket from 'ws';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
