edab955d6ed211b190745c7325b93050e725e331
David Hoernke <103465550+DavidHoernke@users.noreply.github.com>
2022-11-28 21:52:15 -0500

    Cleaned up code

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -1,7 +1,15 @@
 import {useState } from 'react';
 
 
+
 const SignIn = () => {
+
+        const ws =  new WebSocket('ws://127.0.0.1:2236');
+    
+        function inputToString(document) {
+            return "login:"+document.getElementById("email").value+":"+document.getElementById("password").value;
+        }  
+
 //  going to be using state hooks to handle input 
     const SchoolName = "Queen's"
     return ( 
@@ -11,30 +19,33 @@ const SignIn = () => {
                 <h1>Sign in</h1>   
                     <form>
                         <h3>Enter {SchoolName} email:</h3>
-                         <input type="text" placeholder='netID@queensu.ca' />
+                         <input type="text" placeholder='netID@queensu.ca' id="email" />
                     </form>
                 
                     <form>
                         <h3>Enter password:</h3>
-                         <input type="text" placeholder='Password'/>
-                    </form>                    
+                         <input type="text" placeholder='Password' id="password"/>
+                    </form>
+                    <button type="submit" onClick = {() => ws.send(inputToString(document))}>Sign In</button>                    
             </div>
             
             <div className="form2">
                 <h1>Create Account</h1>
                 <form>
                     <h3>Enter {SchoolName} email:</h3>
-                     <input type="text" placeholder='netID@queensu.ca' />
+                     <input type="text" placeholder='netID@queensu.ca' id="email"/>
+                     
                 </form>
             
                 <form>
                     <h3>Enter password:</h3>
-                     <input type="text" placeholder='Password'/>
+                     <input type="text" placeholder='Password' id="password"/>
                 </form>    
                 <form>
                     <h3>Confirm password:</h3>
                      <input type="text" placeholder='Password'/>
-                </form>                 
+                </form>           
+                <button type="submit" onClick = {() => ws.send(inputToString(document))}>Create Account</button>      
         </div>
             
         </div>
