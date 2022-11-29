9daa881a14a9e21e3d9fbe1edba04240cb409460
David Hoernke <103465550+DavidHoernke@users.noreply.github.com>
2022-11-28 20:12:35 -0500

    Updated some Sign in CSS, added create account

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -3,15 +3,14 @@ import {useState } from 'react';
 
 const SignIn = () => {
 //  going to be using state hooks to handle input 
+    const SchoolName = "Queen's"
     return ( 
         <div className="signin">
-            <div className="Header"><h1>Sign in</h1></div>
-            <h3>Create Account</h3>
             
             <div className="form">
-                
+                <h1>Sign in</h1>   
                     <form>
-                        <h3>Enter email:</h3>
+                        <h3>Enter {SchoolName} email:</h3>
                          <input type="text" placeholder='netID@queensu.ca' />
                     </form>
                 
@@ -20,6 +19,23 @@ const SignIn = () => {
                          <input type="text" placeholder='Password'/>
                     </form>                    
             </div>
+            
+            <div className="form2">
+                <h1>Create Account</h1>
+                <form>
+                    <h3>Enter {SchoolName} email:</h3>
+                     <input type="text" placeholder='netID@queensu.ca' />
+                </form>
+            
+                <form>
+                    <h3>Enter password:</h3>
+                     <input type="text" placeholder='Password'/>
+                </form>    
+                <form>
+                    <h3>Confirm password:</h3>
+                     <input type="text" placeholder='Password'/>
+                </form>                 
+        </div>
             
         </div>
      );
